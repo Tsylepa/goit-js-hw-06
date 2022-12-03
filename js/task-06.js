f558f0666a521a3eecbdc223ec70e5522b522508
Tsylepa <tsylepa@gmail.com>
2022-12-03 15:02:26 +0000

    Only 6 symbols validation

--- js/task-06.js
+++ js/task-06.js
@@ -4,11 +4,11 @@ const validLength = input.dataset.length;
 input.addEventListener("blur", validation);
 
 function validation() {
-  if (this.value.length >= validLength) {
-    this.classList.remove("invalid");
+  if (this.value.length == validLength) {
+    this.classList?.remove("invalid");
     this.classList.add("valid");
   } else {
-    this.classList.remove("valid");
+    this.classList?.remove("valid");
     this.classList.add("invalid");
   }
 }
